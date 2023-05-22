7c107df52d97e46c3e07c9819dab1459ff2ba88a
¨Howard <programadorhaaj.2023@gmail.com>
2023-05-22 09:01:10 -0600

    Cambio en valores de la tabla Insumos

--- insumos.sql
+++ insumos.sql
@@ -1,9 +1,9 @@
 create table insumos(
     insumos_id serial not null,
     insumos_nombre varchar(50) not null,
-    insumos_cantidad_existencia varchar(50) not null,
+    insumos_cantidad_existencia INTEGER not null,
     insumos_marca_producto varchar(50)  not null,
     insumos_nombre_proveedor varchar(50) not null,
-    insumos_precio int not null,
+    insumos_precio decimal(10,2) not null,
     primary key (insumos_id)
 )
